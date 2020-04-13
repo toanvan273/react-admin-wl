1b454afe7b243f4f0d53b2578977fddb8c5421f7
Toan Truong Van (Dev - Tech Implementation - Technology - VCCI) <toan.truong@vndirect.com.vn>
2020-04-13 16:24:59 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource, Login } from "react-admin"; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App=()=>(
+  <> 
+  ABC
+  </>
+)
 export default App;
